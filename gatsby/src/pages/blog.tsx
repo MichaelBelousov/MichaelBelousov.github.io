12c803f7b40604260d301ba182feced01e75a165
Michael Belousov <michael.belousov98@gmail.com>
2019-12-15 14:21:05 -0500

    fixed highlighting bug by resetting init called check

--- gatsby/src/pages/blog.tsx
+++ gatsby/src/pages/blog.tsx
@@ -1,24 +1,26 @@
 import React, { useState, useLayoutEffect, useRef } from 'react';
 import { graphql } from 'gatsby';
+import Helmet from 'react-helmet';
 import Layout from '../components/layout';
 import styles from './blog.module.scss';
 import hljs from 'highlight.js';
 
-window.hljs = hljs;
-
 export default function (props: any) {
-  console.log(props);
   useLayoutEffect(() => {
-    console.log('highlighting...');
+    hljs.initHighlighting.called = false;
     hljs.initHighlighting();
   }, []);
   return (
     <Layout pageTitle="Blog">
+      <Helmet>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/a11y-dark.min.css" integrity="sha256-7L/IK7qUTcgTXtfLAxip5Eo+hnp+pSe5htBCh5pYg6o=" crossorigin="anonymous" />
+      </Helmet>
       {props.data.allMarkdownRemark.edges.map(({node}, key) => (
         <div className={styles.post} key={key}>
           <h1>{node.frontmatter.title}</h1>
           <h3>{node.frontmatter.date}</h3>
           <div dangerouslySetInnerHTML={{ __html: node.html }} />
+          <hr />
         </div>
       ))}
     </Layout>
