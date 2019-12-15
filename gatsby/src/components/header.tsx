3b73a74a4fb65922bc02ff28774c7b4504d610cd
Michael Belousov <michael.belousov98@gmail.com>
2019-12-15 13:17:38 -0500

    fixed components names

--- gatsby/src/components/header.tsx
+++ gatsby/src/components/header.tsx
@@ -5,14 +5,16 @@ import styles from './header.module.scss';
 
 const Header = () => (
   <header>
-    <div className={styles.left}>
-      <Link className={styles.navLink} to="/">Home</Link>
-      <Link className={styles.navLink} to="/work">Work</Link>
-      <Link className={styles.navLink} to="/3d">3D</Link>
-      <Link className={styles.navLink} to="/blog">Blog</Link>
-    </div>
-    <div className={styles.right}>
-      <h1>Mike Belousov</h1>
+    <div className={styles.separate}>
+      <div className={styles.left}>
+        <Link className={styles.navLink} to="/">Home</Link>
+        <Link className={styles.navLink} to="/work">Work</Link>
+        <Link className={styles.navLink} to="/3d">3D</Link>
+        <Link className={styles.navLink} to="/blog">Blog</Link>
+      </div>
+      <div className={styles.right}>
+        <h1>Mike Belousov</h1>
+      </div>
     </div>
     <hr/>
   </header>
