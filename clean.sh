9d0de6b0ed8b9e000f0aba1c51d75df613a71c1b
Michael Belousov <mike.belousov@bentley.com>
2021-01-12 20:59:44 -0500

    add clean.sh and clean the webpack crap that I do because of github pages

---
+++ clean.sh
@@ -0,0 +1,2 @@
+# TODO: generate folders to delete by looking in src/pages
+rm -I $@ *.js *.css *.js.map SentinelBeam/ 3d/ page-data/ 404/ images/ blog/ icons rss.xml manifest.webmanifest index.html favicon-32x32.png chunk-map.json 404.html
