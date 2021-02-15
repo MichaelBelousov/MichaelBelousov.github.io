d8c549eac6f3800a4cbb169ea7cfb6469e73b206
Michael Belousov <mike.belousov@bentley.com>
2021-02-15 18:18:19 -0500

    add dialogue schema checker

--- clean.sh
+++ clean.sh
@@ -1,2 +1,3 @@
 # TODO: generate folders to delete by looking in src/pages
+# NOTE: do not delete godot-json-dialogue-schema-verifier.html!
 rm -I $@ *.js *.css *.js.map SentinelBeam/ 3d/ page-data/ 404/ images/ blog/ icons rss.xml manifest.webmanifest index.html favicon-32x32.png chunk-map.json 404.html
