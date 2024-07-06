38193a1d19eb0a37e3a3840bd54c563cd603ca74
Michael Belousov <michael.belousov98@gmail.com>
2024-07-06 09:28:02 -0400

    why was I so naive, fix full matching regex in find

--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-find -E . -maxdepth 1 -not -regex 'CNAME|nojekyll|src|README|git|clean|bubble|godot-json-dialogue' -exec rm -r {} \;
+find -E . -maxdepth 1 -not -regex '.*(CNAME|nojekyll|src|README|git|clean|bubble|godot-json-dialogue).*' -exec rm -r {} \;
