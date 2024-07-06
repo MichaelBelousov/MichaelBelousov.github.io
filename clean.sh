43158500511ff7cc87b022ffab7052b7d2673b10
Michael Belousov <michael.belousov98@gmail.com>
2024-07-06 09:21:45 -0400

    better clean script

--- clean.sh
+++ clean.sh
@@ -1,3 +1,1 @@
-# TODO: generate folders to delete by looking in src/pages
-# NOTE: do not delete godot-json-dialogue-schema-verifier.html!
-rm -rf -I $@ *.js *.css *.js.map SentinelBeam/ 3d/ page-data/ 404/ images/ blog/ icons rss.xml manifest.webmanifest index.html favicon-32x32.png chunk-map.json 404.html
+find -E . -maxdepth 1 -not -regex 'CNAME|nojekyll|src|README|git|clean|bubble|godot-json-dialogue' -exec rm -r {} \;
